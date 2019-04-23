8e8338266ecb7f92a6cca3f9ed6ce561749fee83
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-04-23 16:00:16 -0400

    functions finsihed

--- src/functions.py
+++ src/functions.py
@@ -3,10 +3,19 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
+def evens(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
+if evens(num):
+    print("Even!")
+else:
+    print("Odd!")
 # YOUR CODE HERE
 
